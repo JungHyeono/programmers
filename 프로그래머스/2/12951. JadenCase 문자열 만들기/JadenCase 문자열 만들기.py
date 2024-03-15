1c51965a10209327546aef55f923f68ac94e1237
JungHyeono <101687010+JungHyeono@users.noreply.github.com>
2024-03-15 10:42:59 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -2,8 +2,5 @@ def solution(s):
     a = s.split(' ')
     result = []
     for i in range(len(a)):
-            if i == 0:
-                result.append(a[i].capitalize())
-            else:
-                result.append(a[i].capitalize())
+            result.append(a[i].capitalize())
     return str(' '.join(result))
