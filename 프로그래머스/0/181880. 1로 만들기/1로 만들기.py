678eb12bc09dc21acd3b93fb8c424144f72a4ae1
JungHyeono <101687010+JungHyeono@users.noreply.github.com>
2024-03-27 10:22:46 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    count = 0
+    for i in num_list:
+        while i > 1:
+            count += 1
+            if i % 2 == 0:
+                i = int(i/2)
+            else:
+                i = int((i-1)/2)
+                
+    return count
