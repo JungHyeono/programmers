5ea593cc44365c751996cf01f0f842bf6757f1d0
JungHyeono <101687010+JungHyeono@users.noreply.github.com>
2024-04-05 01:38:25 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.41 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,5 +1,13 @@
 def solution(n):
-    a,b = 0,1
-    for i in range(n):
-        a,b = b,a+b
-    return b % 1234567
+    # a,b = 0,1
+    # for i in range(n):
+    #     a,b = b,a+b
+    # return b % 1234567
+
+    answer = [0, 1]
+    
+    for i in range(2, n+1):
+        answer.append(answer[i-1] + answer[i-2])
+    
+    return (answer[-1]+answer[-2]) % 1234567
+    
