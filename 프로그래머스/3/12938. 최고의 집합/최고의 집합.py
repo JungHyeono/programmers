912ca244c61d47179cb52f48663567b7cd0f2223
JungHyeono <101687010+JungHyeono@users.noreply.github.com>
2024-04-28 11:19:38 +0900

    [level 3] Title: 최고의 집합, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -0,0 +1,15 @@
+import math
+def solution(n, s):
+    answer = []
+    if n > s :
+        return [-1]
+    
+    for i in range(n):
+        answer.append(math.ceil(s/n))
+        
+    if s % n != 0:
+        for i in range(sum(answer)-s):
+            answer[i] -= 1
+    else: return answer
+        
+    return(answer)
