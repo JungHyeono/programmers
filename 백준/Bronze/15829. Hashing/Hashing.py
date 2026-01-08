ee9b99f80c5032c29a682641e5474d60747609f4
JungHyeonHo <101687010+JungHyeono@users.noreply.github.com>
2026-01-09 00:05:44 +0900

    [Bronze II] Title: Hashing, Time: 32 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,17 @@
+r = 31
+M = 1234567891
+
+L = int(input())
+
+alphabet = {}
+
+for i, ch in enumerate('abcdefghijklmnopqrstuvwxyz', start = 1):
+    alphabet[ch] = i
+
+s = input()
+
+answer = 0
+for i in range(L):
+    answer += ((alphabet[s[i]]*(r**i)) % M)
+    
+print(answer)
